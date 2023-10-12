757104966f0b2fbf6b206483b009b115f197fb17
taichikitano <taichikitano@gmail.com>
2023-10-12 23:55:54 +0900

    Update guidances_controller.rb

--- app/controllers/guidances_controller.rb
+++ app/controllers/guidances_controller.rb
@@ -5,8 +5,15 @@ class GuidancesController < ApplicationController
 
   def index
     @q = Guidance.ransack(params[:q])
-    @guidances = @q.result
-    @mainsurf_points = SurfPoint.all.order("id ASC").limit(3)
+    @mainsurf_points = SurfPoint.roots.order("id ASC")
+    
+    if params[:q].present? && params[:q][:surf_point_id_in].present?
+      selected_surf_point = SurfPoint.find(params[:q][:surf_point_id_in])
+      related_surf_point_ids = selected_surf_point.subtree_ids
+      @guidances = @q.result.where(surf_point_id: related_surf_point_ids)
+    else
+      @guidances = @q.result
+    end
   end
 
   def new
@@ -24,7 +31,6 @@ class GuidancesController < ApplicationController
   def create
     @guidance = Guidance.new(guidance_params)
     if @guidance.save
-      binding.pry
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
